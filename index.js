88aeb8012444801b7d2fcc484fc41061eb7d7d46
patrickofthenine <patrickaspeiser@gmail.com>
2019-06-10 21:25:32 -0400

    shuffling answer bank

--- index.js
+++ index.js
@@ -13,6 +13,8 @@ function getQuestionBank(){
 		data: config.params,
 		success: function(results){
 			self.questions = results.results;
+			console.log(self.questions);
+			formatQuestionBank(self.questions);
 			return self.questions;
 		},
 		error: function(error){
@@ -20,6 +22,73 @@ function getQuestionBank(){
 		}
 	});
 };
+
+function shuffle(array){
+	var currentIndex = array.length, temporaryValue, randomIndex;
+
+	  // While there remain elements to shuffle...
+	  while (0 !== currentIndex) {
+
+	    // Pick a remaining element...
+	    randomIndex = Math.floor(Math.random() * currentIndex);
+	    currentIndex -= 1;
+
+	    // And swap it with the current element.
+	    temporaryValue = array[currentIndex];
+	    array[currentIndex] = array[randomIndex];
+	    array[randomIndex] = temporaryValue;
+	}
+	return array;
+}
+
+function shuffleAnswerBank(question){
+	this.question = question;
+	//shuffle answers using fisher yates shuffle SO https://stackoverflow.com/questions/2450954/how-to-randomize-shuffle-a-javascript-array/31054543
+	this.shuffledAnswers = shuffle(this.question.incorrect_answers);
+	//get a random index for our correct answer yet to be added
+	this.correctIndex = Math.floor(Math.random() * this.shuffledAnswers.length+1);
+	//add correct answer to shuffled answer bank
+	this.shuffledAnswers.splice(this.correctIndex, 0, this.question.correct_answer)
+	this.answers = {
+		correct: this.correctIndex,
+		answers: this.shuffledAnswers
+	};
+	return this.answers;
+}
+
+function formatQuestionBank(questionBank){
+	this.questionBank = questionBank;
+	if(!questionBank) 
+		return;
+	
+	this.formatted = [];
+	for(let i = 0; i < this.questionBank.length; i++){
+		this.question = this.questionBank[i];
+		this.question['formatted_answers'] = shuffleAnswerBank(this.question);
+		console.log('TQ', this.question);
+	}	
+}
+
+
+let questionMain = Vue.component('question-main', {
+	props: ['question'],
+	template: `
+		<div>
+			<question-header v-bind:question="question.question"></question-header>
+			<question-answers v-bind:answers="question.incorrect_answers"></question-answers>
+		</div>
+	`
+});
+
+let questionHeader = Vue.component('question-header', {
+	props: ['question'],
+	template: '<div>HEADER {{question}} </div>'
+})
+
+let questionAnswers = Vue.component('question-answers', {
+	props: ['answers'],
+	template: '<div> BODY {{answers}} </div>'
+})
 
 new Vue({
 	el: '#trivia',
@@ -30,8 +99,3 @@ new Vue({
 	},
 	beforeCreate: getQuestionBank
 });
-
-Vue.component('question-main', {
-	props: ['question'],
-	template: '<div> Q {{question.question}} </div>',
-})
